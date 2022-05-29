aee10d342d676b7c7aac2b731c2c222c7c24ef28
Devsgeeknerd <vanderley.1109@gmail.com>
2022-05-29 16:01:38 -0300

    adicionando selenium common exceptions

--- likeiro.py
+++ likeiro.py
@@ -2,3 +2,4 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions
