2a75b0f8a2de1076fe4f2eeb049045bd000f8fb5
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-15 16:25:31 -0300

    adicionando support

--- likeiro.py
+++ likeiro.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
