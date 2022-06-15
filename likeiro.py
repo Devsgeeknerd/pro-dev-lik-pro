ab7f718c1d73c68152a2b32141256a3317e38156
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-15 16:27:09 -0300

    adicionando EC

--- likeiro.py
+++ likeiro.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.support
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
