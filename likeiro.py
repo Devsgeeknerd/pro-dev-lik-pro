a5f8a6059c8eba177e5c763e88c4b95f41b763c2
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-15 16:29:05 -0300

    adicionando tudo

--- likeiro.py
+++ likeiro.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions
+from selenium.common.exceptions import *
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
