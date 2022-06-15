be80781978c45941f18bcf2eacf92a959367b314
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-15 16:23:52 -0300

    adicionando webdriverwait

--- likeiro.py
+++ likeiro.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui
+from selenium.webdriver.support.ui import WebDriverWait
