5dfd7b3f8d0befacdbc8a10d1dbbeebec8372222
Devsgeeknerd <vanderley.1109@gmail.com>
2022-06-15 16:31:36 -0300

    adicionando random

--- likeiro.py
+++ likeiro.py
@@ -5,3 +5,4 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+import random
