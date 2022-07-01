1b9e3b25d09b7d40a25af0313a1099901c1543c2
Devsgeeknerd <vanderley.1109@gmail.com>
2022-07-01 15:55:48 -0300

    adicionando disable gpu

--- likeiro.py
+++ likeiro.py
@@ -19,3 +19,4 @@ class likeiro:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--lang=pt-BR")
+        chrome_options.add_argument("--disable-gpu")
