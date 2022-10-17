3c1e692259ffb099a342c8fa51671e8a918d1cd0
Paulo Vanderley <vanderley.1109@gmail.com>
2022-10-17 18:14:52 -0300

    adicionando keys

--- likeiro.py
+++ likeiro.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 __version__ = "0.0.0.1"
 
