27c033a295db00834e8d94f27c7319a1f386bcec
Paulo Vanderley <vanderley.1109@gmail.com>
2022-10-17 18:09:27 -0300

    adicionando support ui

--- likeiro.py
+++ likeiro.py
@@ -5,6 +5,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui
 
 __version__ = "0.0.0.1"
 
