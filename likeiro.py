2458d8d95e9d8aef370a524988169871e06e21c4
Paulo Vanderley <vanderley.1109@gmail.com>
2022-10-17 17:56:49 -0300

    adicionando notifications

--- likeiro.py
+++ likeiro.py
@@ -13,3 +13,4 @@ class likeiro:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--lang=pt-BR")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-notifications")
