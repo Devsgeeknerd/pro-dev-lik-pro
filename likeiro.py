cee63dec44a6cbc7a028564a49d57e6825632a82
Paulo Vanderley <vanderley.1109@gmail.com>
2022-10-17 17:08:27 -0300

    adicionando comentario

--- likeiro.py
+++ likeiro.py
@@ -1,2 +1,4 @@
 # Curtir fotos e videos no feed do Instagram, Facebook, Twitter e LinkedIn.
 # Fazer as mudanças necessárias de acordo com a rede social preferida.
+
+# Importando as bibliotecas necessárias.
